d9bf9857332eddee47ad0c26061c32df2d9742a6
Taylor Spara <871570719@qq.com>
2025-02-24 16:40:09 +0800

    Update hydra_test.py

--- scripts/hydra_test.py
+++ scripts/hydra_test.py
@@ -72,59 +72,7 @@ def run(cfg:DictConfig):
     # Hydra run directory
     hydra_dir = Path(HydraConfig.get().run.dir)
 
-    # js_tag = 'MPtrj_2022_9_full.json'
-    # js_tag = 'm3gnet_mpf.json'
-    # path = str(os.path.join(
-    #     '/home/huangjiao/anaconda3/envs/diffcsp2/lib/python3.8/site-packages/jarvis/db', js_tag))
-    # with open(path, "r", encoding="utf-8") as f:
-    #     data = json.load(f)
-    # data_list = []
-    # for k, v in data.items():
-    #     for k1, v1 in v.items():
-    #         v1['mptrj_id'] = k1
-    #         data_list.append(v1)
-    # dat = data_list
-
-    # scaler = torch.load(Path(
-    #     "/home/huangjiao/codes/stableMPP/outputs/hydra/singlerun/2024-10-09/11-18-36'}/lm_fn_lnnew") / 'prop_scaler.pt')
-    # one_std_list = []
-    # two_std_list = []
-    # three_std_list = []
-    # more_std_list = []
-
-    # for i in range(len(data_list)):
-    #     material = data_list[i]
-    #     if abs(material['ef_per_atom'] - scaler.means) < abs(scaler.stds):
-    #         one_std_list.append(i)
-    #     elif abs(material['ef_per_atom'] - scaler.means) < 2*abs(scaler.stds):
-    #         two_std_list.append(i)
-    #     elif abs(material['ef_per_atom'] - scaler.means) < 3*abs(scaler.stds):
-    #         three_std_list.append(i)
-    #     else:
-    #         more_std_list.append(i)
-
-    # data = jdata('megnet')
-    # id_train, id_val, id_test = get_id_train_val_test(
-    #     # total_size=len(data),
-    #     # total_size=1580395,
-    #     total_size=cfg.data.total_size,
-    #     split_seed=cfg.train.random_seed,
-    #     train_ratio=cfg.data.train_ratio,
-    #     val_ratio=cfg.data.val_ratio,
-    #     test_ratio=cfg.data.test_ratio,
-    #     n_train=cfg.data.n_train,
-    #     n_test=cfg.data.n_test,
-    #     n_val=cfg.data.n_val,
-    #     keep_data_order=cfg.data.keep_data_order,
-    # )
-    # path = cfg.data.root_path + '/seed_' + str(cfg.train.random_seed) + '_numtest'
-    # filenames = [path + '_train.pkl', path + '_val.pkl', path + '_test.pkl']
-
-    # save_lists([id_train, id_val, id_test], filenames)
-    # loaded_lists = load_lists(filenames)
-
-    # Instantiate datamodule
-    # cfg.data.datamodule.datasets.train.save_path = cfg.data.root_path+r'/train_ori_matformer.pt'
+    
     datamodule: pl.LightningDataModule = hydra.utils.instantiate(
         cfg.data.datamodule, _recursive_=False
     )
@@ -154,7 +102,7 @@ def run(cfg:DictConfig):
 
     # Logger instantiation/configuration
     wandb_logger = None
-    wandb.login(key="29e839bd27fe2c8a0058a0e0cf94f8d9e39b7a93")
+    wandb.login(key="")
     if "wandb" in cfg.logging:
         hydra.utils.log.info("Instantiating <WandbLogger>")
         wandb_config = cfg.logging.wandb
@@ -202,22 +150,7 @@ def run(cfg:DictConfig):
     hydra.utils.log.info("Starting testing!")
     trainer.test(datamodule=datamodule)
 
-    # name = 'Formation energy train'
-    # prop = 'e_form'
-    # save_path = '/home/huangjiao/codes/stableMPP/data/megnet/train_ori.pt'
-    # niggli = True
-    # primitive = False
-    # graph_method = 'crystalnn'
-    # tolerance = 0.1
-    # use_space_group = False
-    # use_pos_index = False
-    # lattice_scale_method = 'scale_length'
-    # preprocess_workers = 30
-    # dataset_name = 'megnet'
-    #
-    #
-    # train_dataset = CrystDataset(name,  prop, niggli, primitive, graph_method, preprocess_workers,
-    #              lattice_scale_method, save_path, tolerance, use_space_group, use_pos_index, dataset_name)
+   
     pass
 
 
